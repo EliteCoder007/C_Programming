be88e608576fccf2de2af9b69491b65a1f3d33b8
Hridoy <ridoyking9999@gmail.com>
2022-07-09 10:27:04 +0600

    Add someStirng

---
+++ Chapter-8.Strings/first&lastname.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void printFullName(char arr[]);
+
+int main(){
+    char firstName[] = "Nafiza";
+    char lastName[] = "Khanom";
+
+    printFullName(firstName);
+    printFullName(lastName);
+
+    return 0;
+}
+
+void printFullName(char arr[]){
+    for(int i = 0; arr[i] != '\0' ;i++){
+        printf("%c", arr[i]);
+    }
+    printf("\n");
+}
