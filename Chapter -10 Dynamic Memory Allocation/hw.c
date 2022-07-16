3a63a9e41932d3e12e4ade2ba91ec8414ff99b25
Hridoy <ridoyking9999@gmail.com>
2022-07-16 23:39:07 +0600

    Finishing C with Dynamic Memory Allocation

---
+++ Chapter -10 Dynamic Memory Allocation/hw.c
@@ -0,0 +1,9 @@
+// a. Search about what is a 'dangling pointer' in C.
+
+// 11
+
+// c. Search about what is 'memory leak' in C.
+// b. Allocate memory for 500 integers using calloc &
+// then store first 500 natural numbers in that space.
+
+// d. Search & find out which is better malloc( ) or calloc ( ).
